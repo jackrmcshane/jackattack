8a9c4c919bab7bcdc729ac250bf0b0b325d667bb
jack <darkmatterdontmatter@protonmail.com>
2022-11-17 15:26:01 -0600

    first commit

---
+++ jackattack.py
@@ -0,0 +1,43 @@
+
+
+
+
+class JackAttack(object):
+
+    """An attack class for Jack"""
+
+    def __init__(self):
+        """Instantiate a Jack"""
+        
+
+
+    def attack(self):
+        """
+        Initiate a Jack Attack
+        """
+        for i in range(3):
+            print("Jack attacks you.")
+
+        print("You die.")
+
+
+
+    def run(self):
+        """
+        Initiate Jack 'Run Mode'
+        """
+        print("You scare Jack.")
+        print("He runs away.")
+
+
+
+
+
+
+def main():
+    jack = JackAttack()
+    jack.attack()
+
+
+if __name__ == "__main__":
+    main()
